3b3c9925a89cf26b9434c80694799d3a373ac663
fathimarafa1503 <fathimarafa1503@gmail.com>
2023-03-03 15:49:46 +0530

    created home

---
+++ pages/home.js
@@ -0,0 +1,14 @@
+import Button from '@/components/Button'
+import Body from '@/model/home/Body'
+import Header from '@/model/home/Header'
+import React from 'react'
+
+export default function home() {
+  return (
+    <div>
+    <Header/>
+    <Body/>
+    </div>
+
+  )
+}
